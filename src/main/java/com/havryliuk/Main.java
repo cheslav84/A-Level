30a45ef09ab41bc8e432e484303e2e446129fa97
Viacheslav Havryliuk <v.cheslav84@gmail.com>
2022-12-25 00:53:57 +0200

    hw15

--- src/main/java/com/havryliuk/Main.java
+++ src/main/java/com/havryliuk/Main.java
@@ -1,5 +1,6 @@
 package com.havryliuk;
 
+import com.havryliuk.container.CarList;
 import com.havryliuk.container.GenericContainer;
 import com.havryliuk.model.Car;
 import com.havryliuk.model.CarType;
@@ -7,32 +8,69 @@ import com.havryliuk.repository.CarArrayRepository;
 import com.havryliuk.service.CarService;
 import com.havryliuk.util.AlgorithmUtil;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
 
-        CarArrayRepository repository = new CarArrayRepository();
-        CarService service = new CarService(repository);
+        CarList<Car> cars = new CarList<>();
+        CarService service = new CarService(new CarArrayRepository());
+        Car firstCar = service.create(CarType.TRUCK);
+        Car secondCar = service.create(CarType.CAR);
 
-        GenericContainer<Car> container = new GenericContainer<>();
+        System.out.println("size = " + cars.size());
+        cars.add(service.create(CarType.CAR));
+        cars.add(service.create(CarType.TRUCK));
+        cars.add(service.create(CarType.TRUCK));
 
-        Car passengerCar = service.create(CarType.CAR);
-        Car truck = service.create(CarType.TRUCK);
-        String wrongCar = "Wrong Car";
+        System.out.println("~~~~~~List of cars:");
+        cars.addFirst(firstCar);
+        cars.forEach(System.out::println);
 
+        System.out.println("\n" + "~~~~~~The first car in the list (after adding a car to the first position):");
+        System.out.println(cars.get(0));
+        System.out.println("~~~~~~Receive a car from the center of the list:");
+        System.out.println(cars.get(1));
+        System.out.println("~~~~~~Receive the last car from the list:");
+        System.out.println(cars.get(cars.size()-1) + "\n");
 
-        container.increaseCount(passengerCar);
-        container.increaseCount(truck);
+        System.out.println("~~~~~~Setting a car to the second position (received car):");
+        System.out.println(cars.set(1, secondCar));
+        System.out.println("~~~~~~Getting a car from the second position:");
+        System.out.println(cars.get(1) + "\n");
 
-        container.print(passengerCar);
-        container.print(truck);
-//        container.print(wrongCar);// compilation error
+        System.out.println("~~~~~~List of cars:");
+        cars.forEach(System.out::println);
+        System.out.println("~~~~~~Getting the number position:");
+        System.out.println(cars.find(firstCar));
+        System.out.println("~~~~~~Getting the number position:");
+        System.out.println(cars.find(secondCar));
 
-        container.increaseCount(passengerCar, 150);
-        container.increaseCount(truck , 20.32);
+        System.out.println("\n" + "~~~~~~List before removing:");
+        cars.forEach(System.out::println);
+        System.out.println("~~~~~~Remove from center");
+        cars.remove(1);
+        System.out.println("~~~~~~Remove fist");
+        cars.remove(0);
+        System.out.println("~~~~~~Remove last");
+        cars.remove(1);
+        System.out.println("List of cars:");
+        cars.forEach(System.out::println);
 
-        container.print(passengerCar);
-        container.print(truck);
+        System.out.println("~~~~~~Create new car list");
+        cars.clear();
+        cars.add(service.create(CarType.CAR));
+        cars.add(service.create(CarType.TRUCK));
+        cars.add(service.create(CarType.TRUCK));
 
+        cars.get(0).setCount(10);
+        cars.get(1).setCount(20);
+        cars.get(2).setCount(30);
+
+        System.out.println("Count fo all cars = " + cars.getCountAllCars());
     }
 
 }
---
+++ src/main/java/com/havryliuk/container/CarList.java
@@ -0,0 +1,224 @@
+package com.havryliuk.container;
+
+import com.havryliuk.model.Car;
+
+import java.util.*;
+
+public class CarList<E extends Car> extends AbstractList<E> {
+    private Node<E> firstNode;
+    private Node<E> lastNode;
+    private int size;
+
+
+    public int getCountAllCars(){
+        int count = 0;
+        Node<E> target = firstNode;
+        for (int i = 0; i < size; i++) {
+            count += target.data.getCount();
+            target = target.next;
+        }
+        return count;
+    }
+
+    public boolean add(E e) {
+        final Node<E> last = lastNode;
+        final Node<E> newNode = new Node<>(e, last, null);
+        lastNode = newNode;
+        if (last == null) {
+            firstNode = newNode;
+        } else {
+            last.next = newNode;
+        }
+        size++;
+        return true;
+    }
+
+
+    public boolean addFirst(E e) {
+        addBefore(0, e);
+        return true;
+    }
+
+    public void add(int index, E element) {
+        checkPositionIndex(index);
+        if (index == size ) {
+            add(element);
+        } else {
+            addBefore(index, element);
+        }
+    }
+
+    public int find (E element) {
+        int counter;
+        Node<E> target = firstNode;
+        for (counter = 0; counter < size; counter++) {
+            if (target.data.equals(element)) {
+                return counter;
+            }
+            target = target.next;
+        }
+        throw new NoSuchElementException();
+    }
+
+    private void addBefore (int index, E element) {
+        Node<E> current = getLinked(index);
+        final Node<E> newNode;
+            Node<E> previous = current.previous;
+            newNode = new Node<>(element, previous, current);
+            current.previous = newNode;
+            if (previous != null)
+                previous.next = newNode;
+            else
+                firstNode = newNode;
+        size++;
+    }
+
+
+
+    public E remove(int index) {
+        checkIndex(index);
+        Node<E> toRemove = getLinked(index);
+        unlink(toRemove);
+        size--;
+        return toRemove.getData();
+    }
+
+    private void unlink(Node<E> current) {
+        if (current.previous == null && current.next == null){
+            unlinkAll();
+        } else if (current.previous == null) {
+            unlinkFirst(current);
+        } else if (current.next == null) {
+            unlinkLast(current);
+        } else {
+            unlinkWithin(current);
+        }
+    }
+
+    private void unlinkFirst(Node<E> first) {
+        Node<E> nextNode = first.next;
+        firstNode = nextNode;
+        nextNode.previous = null;
+    }
+
+    private void unlinkWithin(Node<E> current) {
+        Node<E> nextNode = current.next;
+        Node<E> previousNode = current.previous;
+        nextNode.previous = previousNode;
+        previousNode.next = nextNode;
+    }
+
+    private void unlinkLast(Node<E> last) {
+        lastNode = last.previous;
+        lastNode.next = null;
+    }
+
+    private void unlinkAll() {
+        firstNode = null;
+        lastNode = null;
+    }
+
+    private void checkIndex (int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+    private void checkPositionIndex (int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+    public E get(int index) {
+        checkIndex(index);
+        Node<E> target = getLinked(index);
+        return target.getData();
+    }
+
+    private Node<E> getLinked(int index) {
+        Node<E> target = firstNode;
+        for (int i = 0; i < index; i++) {
+            target = target.next;
+        }
+        return target;
+    }
+
+
+
+    public E set(int index, E element) {
+        checkIndex(index);
+        Node<E> toUpdate = getLinked(index);
+        E elementToReturn = toUpdate.getData();
+        toUpdate.setData(element);
+        Node<E> paired = toUpdate.currentElement;
+        paired.setData(element);
+        return elementToReturn;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+
+    public void clear() {
+        unlinkAll();
+        size = 0;
+    }
+
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int counter = 0;
+            Node<E> current = firstNode;
+            @Override
+            public boolean hasNext() {
+                return counter < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E data = current.data;
+                current = current.next;
+                counter++;
+                return data;
+            }
+        };
+    }
+
+    private class Node<E> {
+        private E data;
+        private Node<E> currentElement;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E data, Node<E> previous, Node<E> next) {
+            this.data = data;
+            currentElement = new Node<>(this);
+            this.next = next;
+            this.previous = previous;
+        }
+
+        private Node(Node<E> currentElement) {
+            this.data = currentElement.getData();
+            this.currentElement = currentElement;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+    }
+
+
+
+}
